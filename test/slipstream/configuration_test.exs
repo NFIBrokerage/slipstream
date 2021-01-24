a1dd123c9f51a58c2322dba90c3c1b2f4925a025
Michael Davis <mcarsondavis@gmail.com>
2021-01-24 14:11:10 -0600

    test the config module

---
+++ test/slipstream/configuration_test.exs
@@ -0,0 +1,29 @@
+defmodule Slipstream.ConfigurationTest do
+  use ExUnit.Case, async: true
+
+  alias Slipstream.Configuration, as: Config
+
+  test "empty config fails validation" do
+    assert {:error, %NimbleOptions.ValidationError{}} = Config.validate([])
+    assert_raise NimbleOptions.ValidationError, fn -> Config.validate!([]) end
+  end
+
+  describe "given a valid URI" do
+    setup do
+      [uri: "ws://localhost/socket/websocket?foo=bar"]
+    end
+
+    test "config with just URI passes, and ws protocol defaults to port 80", c do
+      assert {:ok, %Config{} = config} = Config.validate(uri: c.uri)
+
+      assert match?(%URI{port: 80, scheme: "ws"}, config.uri)
+    end
+
+    test "WSS is assumed to be port 443", c do
+      uri = String.replace(c.uri, ~r/^ws/, "wss")
+      assert {:ok, %Config{} = config} = Config.validate(uri: uri)
+
+      assert match?(%URI{port: 443, scheme: "wss"}, config.uri)
+    end
+  end
+end
