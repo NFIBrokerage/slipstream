8d44dd057f6729b3d34787b85a57f0a968120946
Michael Davis <mcarsondavis@gmail.com>
2021-02-07 09:54:49 -0600

    slipstream basics

--- examples/graceful_startup/client.ex
+++ examples/graceful_startup/client.ex
@@ -1,2 +1,12 @@
 defmodule MyApp.GracefulStartupClient do
+  use Slipstream
+
+  @moduledoc """
+  A slipstream client that gracefully handles misconfiguration errors on
+  start-up
+  """
+
+  def start_link(opts) do
+    Slipstream.start_link(__MODULE__, opts, name: __MODULE__)
+  end
 end
