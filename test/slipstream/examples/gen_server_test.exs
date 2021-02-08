290d72fa527c81fc7831a99e9e38ecb09e840c23
Michael Davis <mcarsondavis@gmail.com>
2021-02-07 21:09:29 -0600

    add a test file

---
+++ test/slipstream/examples/gen_server_test.exs
@@ -0,0 +1,22 @@
+defmodule Slipstream.GenServerTest do
+  use Slipstream.SocketTest
+
+  setup do
+    client = MyApp.GenServerClient
+
+    start_supervised!({client, uri: "ws://localhost", test_mode?: true})
+
+    [client: client]
+  end
+
+  test "casting a topic join results in the client asking to join", c do
+    accept_connect(c.client)
+
+    topic = "rooms:lobby"
+    params = %{"fizz" => "buzz"}
+
+    GenServer.cast(c.client, {:join, topic, params})
+
+    assert_join ^topic, ^params, :ok
+  end
+end
