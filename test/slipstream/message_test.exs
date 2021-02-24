124214982d99ba357b6537e4f3325c17a91bf0f3
Michael Davis <mcarsondavis@gmail.com>
2021-02-24 14:08:53 -0600

    use string keys in malformed message test

--- test/slipstream/message_test.exs
+++ test/slipstream/message_test.exs
@@ -6,10 +6,10 @@ defmodule Slipstream.MessageTest do
   test "from_map!/1 raises a KeyError on a malformed message" do
     # malformed because there is no payload key
     malformed_message = %{
-      topic: "rooms:lobby",
-      event: "msg:new",
-      ref: nil,
-      join_ref: nil
+      "topic" => "rooms:lobby",
+      "event" => "msg:new",
+      "ref" => nil,
+      "join_ref" => nil
     }
 
     assert_raise KeyError, fn -> Message.from_map!(malformed_message) end
