00f5821a93e8fc9d9cf9d3c3692a9c910977824e
Michael Davis <mcarsondavis@gmail.com>
2021-06-30 10:36:41 -0500

    switch out gun for mint_web_socket dep

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Slipstream.MixProject do
 
   defp deps do
     [
-      {:gun, "~> 1.0"},
+      {:mint_web_socket, path: "../mint_web_socket"},
       {:telemetry, "~> 0.4"},
       {:jason, "~> 1.0", optional: true},
       {:nimble_options, "~> 0.1"},
